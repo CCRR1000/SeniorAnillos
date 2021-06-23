a66cfd46a28b16cd231371465f9895cc4bd5e1cd
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-23 14:49:07 -0600

    Creacion clase Humano

---
+++ src/Personajes/Heroes/Humano.java
@@ -0,0 +1,12 @@
+package src.Personajes.Heroes;
+
+public class Humano extends Heroe {
+
+    public Humano(String nombre) {
+        super(nombre, 180);
+    }
+
+    public Humano() {
+    }
+    
+}
