7d72a68a6e334bd967c171f55a7cc809be0df133
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-23 14:46:41 -0600

    Creacion clase Trasgo

---
+++ src/Personajes/Bestias/Trasgo.java
@@ -0,0 +1,12 @@
+package src.Personajes.Bestias;
+
+public class Trasgo extends Bestia {
+
+    public Trasgo(String nombre) {
+        super(nombre, 325);
+    }
+
+    public Trasgo() {
+    }
+    
+}
