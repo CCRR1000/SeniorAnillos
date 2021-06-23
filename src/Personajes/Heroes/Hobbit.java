e02b0c70135fdcf43a5aeb9de2be22df916e217b
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-23 14:48:52 -0600

    Creacion clase Hobbit

---
+++ src/Personajes/Heroes/Hobbit.java
@@ -0,0 +1,13 @@
+package src.Personajes.Heroes;
+
+public class Hobbit extends Heroe {
+
+    public Hobbit(String nombre) {
+        super(nombre, 200);
+    }
+
+    public Hobbit() {
+    }
+ 
+    
+}
