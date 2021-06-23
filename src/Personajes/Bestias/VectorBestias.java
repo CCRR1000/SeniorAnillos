35f006b7ea2ef4aa7ccf3c7169d1f100fd22fb5e
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-23 14:47:05 -0600

    Creacion clase VectorBestias

---
+++ src/Personajes/Bestias/VectorBestias.java
@@ -0,0 +1,30 @@
+package src.Personajes.Bestias;
+
+public class VectorBestias {
+
+    private Bestia[] bestias = {new Orco("Ghethrof"), new Orco("Srocri"), new Orco("Krarzegh"), new Orco("Bruzgaf"), 
+                                new Orco("Gorged"), new Trasgo("Uthru"), new Trasgo("Augdu"), 
+                                new Trasgo("Augzush"), new Trasgo("Aucbo"), new Trasgo("Auharg")};
+
+    public VectorBestias() {
+
+        
+
+
+/* 
+        Bestia orco1 = new Orco("Ghethrof");
+        Bestia orco2 = new Orco("Srocri");
+        Bestia orco3 = new Orco("Krarzegh");
+        Bestia orco4 = new Orco("Bruzgaf");
+        Bestia orco5 = new Orco("Gorged");
+
+        Bestia trasgo1 = new Trasgo("Uthru");
+        Bestia trasgo2 = new Trasgo("Augdu");
+        Bestia trasgo3 = new Trasgo("Augzush");
+        Bestia trasgo4 = new Trasgo("Aucbo");
+        Bestia trasgo5 = new Trasgo("Auharg");
+
+        Bestia[] bestias = { orco1, orco2, orco3, orco4, orco5, trasgo1, trasgo2, trasgo3, trasgo4, trasgo5 };
+ */
+    }
+}
