26435d894239188f44302207b0ab118b983464b3
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-23 23:41:46 -0600

    Clase Finalizada

--- src/Juego/Principal.java
+++ src/Juego/Principal.java
@@ -6,19 +6,9 @@ import src.Personajes.Heroes.*;
 
 public class Principal {
     public static void main(String[] args) {
-        Juego j = new Juego();
-        Rand r = new Rand();
-        Bestia b = new Bestia();
-        Orco o = new Orco();
-        Trasgo t = new Trasgo();
-        VectorBestias v = new VectorBestias();
-        Elfo e = new Elfo();
-        Heroe h = new Heroe();
-        Hobbit i = new Hobbit();
-        Humano u = new Humano();
-        Personaje p = new Personaje();
 
-        System.out.println("\n\n   COMPILADO\n\n");
+        Juego juego = new Juego();
+        juego.inicializarJuego();
         
     }
 }
